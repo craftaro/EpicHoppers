66e084ca65d8a1bf9f4ce432c49896bab1f74476
Brianna O'Keefe <bri@songoda.com>
2018-08-13 10:58:58 -0400

    Expanded on the API documentation.

--- EpicHoppers-API/src/main/java/com/songoda/epichoppers/api/hopper/LevelManager.java
+++ EpicHoppers-API/src/main/java/com/songoda/epichoppers/api/hopper/LevelManager.java
@@ -5,15 +5,17 @@ import java.util.Map;
 public interface LevelManager {
 
     /**
-     * This will add a level to the level manager. These levels can be used when upgrading hoppers.
+     * This will add a level to the level manager. These levels can be
+     * used when upgrading hoppers or giving hoppers with the built in
+     * "give" command.
      *
-     * @param level
-     * @param costExperiance
-     * @param costEconomy
-     * @param range
-     * @param amount
-     * @param suction
-     * @param blockBreak
+     * @param level The level of the hopper
+     * @param costExperiance  The cost in experience to upgrade the hopper
+     * @param costEconomy The cost in economy to upgrade the hopper
+     * @param range The range in which this hopper will need to be in order to sync with another hopper
+     * @param amount The amount of items this hopper will transfer at a single time
+     * @param suction The distance in which this hopper will suck items into it
+     * @param blockBreak The tick frequency in which this hopper will break blocks placed directly above it.
      */
     void addLevel(int level, int costExperiance, int costEconomy, int range, int amount, int suction, int blockBreak);
 
