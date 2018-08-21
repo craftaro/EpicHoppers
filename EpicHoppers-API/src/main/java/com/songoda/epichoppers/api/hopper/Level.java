5699e77fd80fd33e30990060101f30c286f2a6a9
Brianna O'Keefe <bri@songoda.com>
2018-08-21 14:36:41 -0400

    Expanded API

--- EpicHoppers-API/src/main/java/com/songoda/epichoppers/api/hopper/Level.java
+++ EpicHoppers-API/src/main/java/com/songoda/epichoppers/api/hopper/Level.java
@@ -3,23 +3,90 @@ package com.songoda.epichoppers.api.hopper;
 import java.util.List;
 
 public interface Level {
+
+    /**
+     * Get a list of strings describing the various
+     * levels and their details that are currently
+     * unlocked with this level.
+     *
+     * @return list of levels
+     */
     List<String> getDescription();
 
+    /**
+     * Get the current level in numerical format.
+     *
+     * @return level
+     */
     int getLevel();
 
+    /**
+     * Get the range that this Level will allow
+     * the applied hopper to remotely connect with
+     * another hopper.
+     *
+     * @return range
+     */
     int getRange();
 
+    /**
+     * Get the amount of items that will transfer
+     * between this hopper and the remotely connected
+     * hopper at a single time.
+     *
+     * @return amount
+     */
     int getAmount();
 
+    /**
+     * Get the tick speed of the BlockBreaking ability
+     * for this level.
+     *
+     * 0 means disabled.
+     *
+     * @return BlockBreak tick speed
+     */
     int getBlockBreak();
 
+    /**
+     * Whether or not the filter is enabled with this
+     * level.
+     *
+     * @return true if the filter is enabled, false
+     * otherwise
+     */
     boolean isFilter();
 
+    /**
+     * Whether or not teleporting through hoppers is
+     * enabled with this level.
+     *
+     * @return true if teleporting is enabled false
+     * otherwise
+     */
     boolean isTeleport();
 
+    /**
+     * Get the distance in which a hopper with this
+     * level will suck items into them.
+     *
+     * @return suction amount
+     */
     int getSuction();
 
+    /**
+     * Get the cost in experience in order to upgrade
+     * to this level.
+     *
+     * @return experience upgrade cost
+     */
     int getCostExperience();
 
+    /**
+     * Get the cost in economy in order to upgrade
+     * to this level.
+     *
+     * @return economy upgrade cost
+     */
     int getCostEconomy();
 }
