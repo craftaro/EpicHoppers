8224cccd5081da97253e95712ead04f34b56b511
Brianna <bri@songoda.com>
2019-04-15 21:57:00 -0400

    quick fix 2

--- EpicHoppers-Plugin/src/main/java/com/songoda/epichoppers/handlers/HopHandler.java
+++ EpicHoppers-Plugin/src/main/java/com/songoda/epichoppers/handlers/HopHandler.java
@@ -136,13 +136,11 @@ public class HopHandler {
                         if (!whiteList.isEmpty()
                                 && whiteList.stream().noneMatch(itemStack -> itemStack.isSimilar(hopperContents[finalIncrement]))) {
                             doBlacklist(hopperState, hopper, hopperContents[i].clone(), amount, i);
-                            block.getState().update();
                             continue main;
                         }
 
                         if (blackList.stream().noneMatch(itemStack -> itemStack.isSimilar(hopperContents[finalIncrement]))) {
                             if (addItem(hopperState, hopper, destinationInventory, destinationBlock, hopperContents[i], amount, i)) {
-                                block.getState().update();
                                 continue main;
                             }
                         }
