dc013a18f73b89030d65e974c8a2a88f5f03ab96
Brianna <bri@songoda.com>
2019-05-27 17:06:12 -0400

    Fix for burning furnaces 1.12-

--- src/main/java/com/songoda/epichoppers/tasks/HopTask.java
+++ src/main/java/com/songoda/epichoppers/tasks/HopTask.java
@@ -180,32 +180,32 @@ public class HopTask extends BukkitRunnable {
 
         Inventory destinationInventory = currentDestination.getInventory();
 
-        switch (destinationType) {
-            case ENDER_CHEST:
+        switch (destinationType.name()) {
+            case "ENDER_CHEST":
                 OfflinePlayer op = Bukkit.getOfflinePlayer(hopper.getPlacedBy());
 
                 if (op.isOnline())
                     destinationInventory = op.getPlayer().getEnderChest();
                 break;
-            case BLACK_SHULKER_BOX:
-            case BLUE_SHULKER_BOX:
-            case BROWN_SHULKER_BOX:
-            case CYAN_SHULKER_BOX:
-            case GRAY_SHULKER_BOX:
-            case GREEN_SHULKER_BOX:
-            case LIGHT_BLUE_SHULKER_BOX:
-            case LIGHT_GRAY_SHULKER_BOX:
-            case LIME_SHULKER_BOX:
-            case MAGENTA_SHULKER_BOX:
-            case ORANGE_SHULKER_BOX:
-            case PINK_SHULKER_BOX:
-            case PURPLE_SHULKER_BOX:
-            case RED_SHULKER_BOX:
-            case SHULKER_BOX:
-            case WHITE_SHULKER_BOX:
-            case YELLOW_SHULKER_BOX:
+            case "BLACK_SHULKER_BOX":
+            case "BLUE_SHULKER_BOX":
+            case "BROWN_SHULKER_BOX":
+            case "CYAN_SHULKER_BOX":
+            case "GRAY_SHULKER_BOX":
+            case "GREEN_SHULKER_BOX":
+            case "LIGHT_BLUE_SHULKER_BOX":
+            case "LIGHT_GRAY_SHULKER_BOX":
+            case "LIME_SHULKER_BOX":
+            case "MAGENTA_SHULKER_BOX":
+            case "ORANGE_SHULKER_BOX":
+            case "PINK_SHULKER_BOX":
+            case "PURPLE_SHULKER_BOX":
+            case "RED_SHULKER_BOX":
+            case "SHULKER_BOX":
+            case "WHITE_SHULKER_BOX":
+            case "YELLOW_SHULKER_BOX":
                 return false;
-            case BREWING_STAND: {
+            case "BREWING_STAND": {
                 BrewerInventory brewerInventory = (BrewerInventory) destinationInventory;
 
                 int maxSize = itemToMove.getMaxStackSize();
@@ -242,7 +242,9 @@ public class HopTask extends BukkitRunnable {
                 debt(item, amountToMove, currentHolder);
                 return true;
             }
-            case FURNACE: {
+            case "BLAST_FURNACE":
+            case "BURNING_FURNACE":
+            case "FURNACE": {
                 FurnaceInventory furnaceInventory = (FurnaceInventory) destinationInventory;
 
                 boolean isFuel = (plugin.isServerVersionAtLeast(ServerVersion.V1_13) ? item.getType().isFuel() : Methods.isLegacyFuel(item.getType())) && !item.getType().name().contains("LOG");
